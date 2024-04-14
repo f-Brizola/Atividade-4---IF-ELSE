170b94675b63a5ecb88a5c700f7fd2a02558da9e
Brizola <160533718+f-Brizola@users.noreply.github.com>
2024-04-14 10:07:18 -0300

    Create ATIVIDADE-2.java

---
+++ ATIVIDADE-2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        int num = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero: ");
+        numero = scanner.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("O número é par");
+        }else{
+            System.out.println("O número é impar");
+        }
+
+      
+    }
+
+}
